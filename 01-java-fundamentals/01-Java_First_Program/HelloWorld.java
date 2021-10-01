d9df677ba59e47b69a62e3c6578462d2ae4af4c2
ianseki <ian.m.seki@gmail.com>
2021-10-01 11:32:00 -0700

    first project practice

--- 01-java-fundamentals/01-Java_First_Program/HelloWorld.java
+++ 01-java-fundamentals/01-Java_First_Program/HelloWorld.java
@@ -1,6 +1,8 @@
-public class Assignment_1 {
+public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("My name is Ian Seki");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Waipahu, HI");
     }
 }
